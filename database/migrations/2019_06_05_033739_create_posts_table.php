4807a6dc14ef2dfae885669a36b1251df3b57eaa
peter.dai <peter.dai@dadaabc.net>
2019-06-05 14:14:23 +0800

    create post in mysql

---
+++ database/migrations/2019_06_05_033739_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('slug')->unique(); //将文章标题转化为 URL 的一部分，以利于SEO
+            $table->string('title');
+            $table->text('content');
+            $table->softDeletes();
+            $table->timestamp('published_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
